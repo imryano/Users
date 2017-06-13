b7e75f0afb1c18ba81eb0d36187aac7ad31fe8bc
Ryan O'Dowd <imryano@gmail.com>
2017-06-13 12:53:51 +1000

    Splitting User functionality up more correctly, added DB

--- userRead/main.go
+++ userRead/main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/imryano/Users/userPackage"
 	"github.com/imryano/utils/password"
+	"github.com/imryano/utils/webservice"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"net/http"
 	"time"
 )
@@ -15,7 +18,7 @@ var signingString = []byte("MYrIg08fMld1zBpb8SgddbhgbVLLIxlQS7ihHkWt")
 type userClaims struct {
 	Username    string
 	AccessLevel int
-	ID          int
+	ID          bson.ObjectId `bson:"_id,omitempty"`
 	jwt.StandardClaims
 }
 
@@ -25,38 +28,22 @@ type userCreds struct {
 }
 
 func main() {
-	http.HandleFunc("/tryLogin", tryLogin)
-	http.HandleFunc("/checkUsername", checkUsername)
-	http.ListenAndServe(":8080", nil)
-}
-
-func SetCORSProps(w http.ResponseWriter, origin string) http.ResponseWriter {
-	w.Header().Set("Access-Control-Allow-Origin", origin)
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	return w
-}
-
-func checkUsername(w http.ResponseWriter, r *http.Request) {
-	preflight := (r.Method == "OPTIONS")
-
-	origin := r.Header.Get("Origin")
-	if origin == "" {
-		origin = "http://localhost:8070"
-	}
-
-	w = SetCORSProps(w, origin)
-
-	if !preflight {
-		userExists := true
-		userExistsStr := "true"
-		if userExists {
-			userExistsStr = "true"
+	user := userPackage.User{Username: "imryano", Password: password.HashPassword("password123"), Email: "imryano@gmail.com"}
+	session, err := mgo.Dial(userPackage.DbUrl)
+	if err == nil {
+		result := &userPackage.User{}
+		col := session.DB(userPackage.DbName).C(userPackage.UserCol)
+		err = col.Find(bson.M{"Username": user.Username}).One(result)
+		if err == nil {
+			user.ID = result.ID
+			col.UpdateId(result.ID, user)
 		} else {
-			userExistsStr = "false"
+			col.Insert(user)
 		}
-		w.Write([]byte("{\"userExists\":\"" + userExistsStr + "\"}"))
 	}
+
+	http.HandleFunc("/tryLogin", tryLogin)
+	http.ListenAndServe(":8080", nil)
 }
 
 func tryLogin(w http.ResponseWriter, r *http.Request) {
@@ -79,46 +66,60 @@ func tryLogin(w http.ResponseWriter, r *http.Request) {
 			username := CheckToken(cookie.Value).Username
 			if username != "" {
 				//Cookie is working fine, can redirect
-				errString += "Cookie already exists. No need to recreate."
-				redirectUrl = "http://www.google.com.au"
+				errString += "Cookie already exists. Clearing. "
 			} else {
 				//Invalid cookie. Clear and prepare for reauthentication
-				cookie.MaxAge = -1
-				http.SetCookie(w, cookie)
-				errString += "Cookie invalid. Clearing."
+				errString += "Cookie invalid. Clearing. "
 			}
+			cookie.MaxAge = -1
+			http.SetCookie(w, cookie)
 		} else {
-
 			fmt.Printf("\nNew Request from: %s\n", origin)
 
+			//Check if user exists
 			uc := userCreds{}
-
 			decoder := json.NewDecoder(r.Body)
 			err := decoder.Decode(&uc)
 
 			if err != nil {
-				errString = "Login Failed: Invalid Request\n"
+				errString += "Login Failed: Invalid Request. "
+			} else {
+				fmt.Printf("Login Request Received with the following creds: \nUsername: %s \nPassword: %s\n", uc.Username, uc.Password)
+
+				session, err := mgo.Dial(userPackage.DbUrl)
+				if err == nil {
+					result := &userPackage.User{}
+					col := session.DB(userPackage.DbName).C(userPackage.UserCol)
+					err = col.Find(bson.M{"username": uc.Username}).One(result)
+					if err == nil && result.Username == uc.Username {
+						fmt.Printf("Request: %s, DB: %s", uc.Username, result.Username)
+						if password.CheckPassword(uc.Password, result.Password) {
+							cookie = &http.Cookie{
+								Name:     "token",
+								Value:    CreateUserCookie(&uc),
+								Expires:  time.Now().Add(time.Hour),
+								MaxAge:   50000,
+								Secure:   false,
+								HttpOnly: true,
+								Raw:      "",
+								Path:     "/",
+							}
+
+							http.SetCookie(w, cookie)
+						} else {
+							errString += "Authentication Failed: Username/Password combination doesn't exist. "
+						}
+					} else {
+						errString += "Authentication Failed: Username/Password combination doesn't exist. "
+					}
+				} else {
+					errString += "Authentication Failed: Could not connect to DB. "
+				}
 			}
-
-			fmt.Printf("Login Request Received with the following creds: \nUsername: %s \nPassword: %s\n", uc.Username, uc.Password)
-
-			cookie = &http.Cookie{
-				Name:     "token",
-				Value:    CreateUserCookie(&uc),
-				Expires:  time.Now().Add(time.Hour),
-				MaxAge:   50000,
-				Secure:   false,
-				HttpOnly: true,
-				Raw:      "",
-				Path:     "/",
-			}
-
-			http.SetCookie(w, cookie)
-			errString += CheckToken(cookie.Value).Username
 		}
 	}
 
-	w = SetCORSProps(w, origin)
+	w = webservice.SetCORSProps(w, origin)
 	w.Write([]byte("{\"error\":\"" + errString + "\", \"redirectURL\":\"" + redirectUrl + "\"}"))
 }
 
